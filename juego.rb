8a27dcbcd024f8ee3adef00c04577b8158da10a7
Pedro Maldonado <pjmg96@gmail.com>
2018-12-22 15:10:44 -0400

    Agregar jugadas

--- juego.rb
+++ juego.rb
@@ -36,3 +36,83 @@ end
 pi = Piedra.new "piedra"
 puts pi
 puts pi.puntos("tijeras").to_s
+
+class Papel < Jugada
+
+	def puntos(j)
+		if j == "piedra"
+			resultado = [1,0]
+		elsif j == "tijeras"
+			resultado = [0,1]
+		elsif j == "spock"
+			resultado = [1,0]
+		elsif j == "lagarto"
+			resultado = [0,1]
+		elsif j == "papel"
+			resultado = [0,0]
+		else
+			raise "Movimiento invalido"
+		end
+		resultado
+	end
+end		
+
+class Tijeras < Jugada
+
+	def puntos(j)
+		if j == "papel"
+			resultado = [1,0]
+		elsif j == "piedra"
+			resultado = [0,1]
+		elsif j == "spock"
+			resultado = [0,1]
+		elsif j == "lagarto"
+			resultado = [1,0]
+		elsif j == "tijeras"
+			resultado = [0,0]
+		else
+			raise "Movimiento invalido"
+		end					
+		resultado
+	end
+end	
+
+class Spock < Jugada
+
+	def puntos(j)
+		if j == "piedra"
+			resultado = [1,0]
+		elsif j == "papel"
+			resultado = [0,1]
+		elsif j == "lagarto"
+			resultado = [0,1]
+		elsif j == "tijeras"
+			resultado = [1,0]
+		elsif j == "spock"
+			resultado = [0,0]
+		else 
+			raise "Movimiento invalido"
+		end
+		resultado
+	end
+end
+
+class Lagarto < Jugada
+
+	def puntos(j)
+		if j == "papel"
+			resultado = [1,0]
+		elsif j == "piedra"
+			resultado = [0,1]
+		elsif j == "tijeras"
+			resultado = [0,1]
+		elsif j == "spock"
+			resultado = [1,0]
+		elsif j == "lagarto"
+			resultado = [0,0]
+		else 
+			raise "Movimiento invalido"
+		end
+		resultado
+	end
+end							
